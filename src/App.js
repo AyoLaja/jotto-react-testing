dc99e4e34dfd5d9c5bbf63d0db2e72aa902c4398
Ayomide Onalaja <ayomide@aminopay.com>
2020-03-16 05:35:28 -0400

    added getSecretWord action vcreator on App mount

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+
 import './App.css';
 import Congrats from './components/Congrats';
 import GuessedWords from './components/GuessedWords';
+import Input from './components/Input'
+import { getSecretWord } from './redux/actions'
 
-class App extends Component {
+export class UnconnectedApp extends Component {
+  componentDidMount() {
+    //Get the secret word
+    this.props.getSecretWord()
+  }
+
   render() {
-    const guessedWords = [
-      { guessedWord: 'train', matchingLetterCount: 2 }
-    ]
     return (
       <div className="App">
         <h1>Jotto</h1>
-        <Congrats success={true}/>
-        <GuessedWords guessedWords={guessedWords}/>
+        <Congrats success={this.props.success}/>
+        <Input/>
+        <GuessedWords guessedWords={this.props.guessedWords}/>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  const { success, guessedWords, secretWord } = state
+
+  return {
+    success, 
+    guessedWords, 
+    secretWord
+  }
+}
+
+const appContainer = connect(
+  mapStateToProps,
+  { getSecretWord }
+)(UnconnectedApp)
+
+export default appContainer;
--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,66 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { shallow } from 'enzyme'
 
-test('renders learn react link', () => {
-});
+import { storeFactory } from '../test/testUtils'
+import App, { UnconnectedApp } from './App';
+
+const setup = (state = {}) => {
+  const store = storeFactory(state)
+  const wrapper = shallow(<App store={store}/>).dive().dive()
+  return wrapper
+}
+
+describe('redux properties', () => {
+  it('has access to `success` state', () => {
+    const success = true 
+    const wrapper = setup({ success })
+    const successProp = wrapper.instance().props.success
+    expect(successProp).toBe(success)
+  })
+
+  it('has access to `secretWord` state', () => {
+    const secretWord = 'party'
+    const wrapper = setup({ secretWord })
+    const secretWordProp = wrapper.instance().props.secretWord
+    expect(secretWordProp).toBe(secretWord)
+  })
+
+  it('has access to `guessedWords` state', () => {
+    const guessedWords = [{ guessedWord: 'train', letterMatchCount: 3 }]
+    const wrapper = setup({ guessedWords })
+    const guessedWordsProp = wrapper.instance().props.guessedWords
+    expect(guessedWordsProp).toEqual(guessedWords)
+  })
+
+  it('has access to `getSecretWord` action creator as a function in props', () => {
+    const wrapper = setup()
+    const getSecretWordProp = wrapper.instance().props.getSecretWord
+    expect(getSecretWordProp).toBeInstanceOf(Function)
+  })
+})
+
+describe('getSecretWord', () => {
+  it('runs on App mount', () => {
+    //Create mock function
+    //Jest will watch to see when this is called and how
+    const getSecretWordMock = jest.fn()
+
+    //Set up app component with getSecretWordMock function as a getSecretWord prop
+    //Not using setup function because that uses connected app
+    const props = {
+      success: false,
+      getSecretWord: getSecretWordMock,
+      guessedWords: []
+    }
+    const wrapper = shallow(<UnconnectedApp {...props}/>)
+
+    //Run lifecycle method
+    wrapper.instance().componentDidMount()
+
+    //Check to see if mock ran
+    const getSecretWordCallCount = getSecretWordMock.mock.calls.length
+
+    expect(getSecretWordCallCount).toBe(1)
+
+  })
+})
